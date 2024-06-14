b0c029c38a2eb9f1a32fcd9a0411a8dd72622a80
KimBohne <kim.bohne@hotmail.com>
2024-06-14 16:11:02 +0200

    uitwerking/variable.js/mathematical.js

--- mathematical-operators.js
+++ mathematical-operators.js
@@ -3,27 +3,43 @@ console.log('Zorg dat al jouw uitkomsten één voor één in de terminal worden 
 // ==========================================
 // 1. Declareer een variabele en sla daar de uitkomst van 4 plus 5 in op.                           // geeft 9 wanneer gelogd
 // ==========================================
-
+const som1 = 4 + 5;
+console.log(som1);
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van 7 maal 52 in op.                          // geeft 364 wanneer gelogd
 // ==========================================
-
+const som2 = 7 * 52;
+console.log(som2);
 
 // ==========================================
 // 3. Declareer een variabele en sla daar de uitkomst van 4 + 3 in op, vermenigvuldigd met 7.       // geeft 49 wanneer gelogd
 // ==========================================
-
+let som3 = (4 + 3) * 7;
+console.log(som3);
 
 // ==========================================
 // 4. Declareer een variabele en sla daar de uitkomst van 36 gedeeld door 6, maal 2 in op.          // geeft 12 wanneer gelogd
 // ==========================================
-
+let som4 = (36 / 6 ) * 2;
+console.log(som4);
 
 // ==========================================
 // 5. Declareer twee variabelen en zet daarin respectievelijk de waardes 'zoet' en 'sappig'.
 // Declareer nog een variabele en sla daarin de samenvoeging van bovenstaande variabelen in op.     // geeft 'zoetsappig' wanneer gelogd
 // ==========================================
+
+const sweet = "zoet";
+const juicy ="sappig";
+const combined = sweet + juicy;
+console.log(combined);
+
+// kijken of dit wel kan met de let variable:
+// let som5EenRegel = "zoet" + "sappig";
+// som5EenRegel = "zuur" + "sappig";
+// console.log(som5EenRegel);
+
+
 
 
 // ==========================================
@@ -31,6 +47,11 @@ console.log('Zorg dat al jouw uitkomsten één voor één in de terminal worden 
 // Declareer vervolgens een variabele voor jouw volledige naam en plak bovenstaande variabelen aan elkaar.
 // Zorg dat er een spatie tussen zit zonder iets aan de originele variabelen te veranderen.        // geeft bijv. 'Henk Pieters' wanneer gelogd
 // ==========================================
+const firstName = "Kim";
+const lastName = "Böhne";
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+
 
 
 // ==========================================
@@ -38,3 +59,7 @@ console.log('Zorg dat al jouw uitkomsten één voor één in de terminal worden 
 // Declareer vervolgens een variabele voor jouw locatie en plak jouw de woonplaats en provincie aan elkaar.
 // Zorg dat er een komma en spatie tussen zit zonder iets aan de originele variabelen te veranderen. // geeft bijv. Maastricht, Limburg wanneer gelogd
 // ==========================================
+const city = "Bussum";
+const provincie = "Noord-Holland";
+const location = city + "," + provincie;
+console.log(location);
